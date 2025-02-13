952ad34cd91df4a1aba3bcf14bf090ad2eab11aa
mabbakk <siyun145928@gmail.com>
2025-02-13 10:46:25 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Layout/Header.jsx";
+import './App.css';
+
+const App = () => {
+
+  return (
+    <>
+        <Header />
+        <div id="main">
+
+        </div>
+    </>
+  )
+}
+
+export default App;
